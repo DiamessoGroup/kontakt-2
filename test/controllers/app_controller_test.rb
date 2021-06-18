f1e0fa97063f68bb9906110eab79cfe9a5d4d271
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-06-17 21:40:53 -0500

    Added Navbar and Home Content

--- test/controllers/app_controller_test.rb
+++ test/controllers/app_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class AppControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get app_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
