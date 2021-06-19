3211278dab5060bd57f975d6056c82ae87200976
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-06-19 01:20:10 -0500

    Added Devise

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'jbuilder', '~> 2.7'
 
 # Rexml
 gem 'rexml'
+
+# Devise
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'app#home'
   get 'app/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
