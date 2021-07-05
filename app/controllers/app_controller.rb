44411af770b177f5d9c31796da2d6d8e4a06fd64
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-07-04 21:17:29 -0500

    Added simplecov to gemfile Authentica user Test for redirect admin when not logged in

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,6 @@
 class AppController < ApplicationController
+  before_action :authenticate_user!, only: :admin
+
   def home
   end
 
--- test/controllers/app_controller_test.rb
+++ test/controllers/app_controller_test.rb
@@ -1,13 +1,20 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AppControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
   end
 
-  test "should get about" do
+  test 'should get about' do
     get app_about_url
     assert_response :success
+  end
+
+  test 'should redirect admin link to login when not logged in' do
+    get admin_path
+    assert_redirected_to user_session_path
   end
 end
