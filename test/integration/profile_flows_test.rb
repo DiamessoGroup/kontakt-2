2b906adc5b26edd58a9ba49fc03f5021356f456e
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-08-05 07:06:56 -0500

    System Test for Creating Profile Added

--- test/integration/profile_flows_test.rb
+++ test/integration/profile_flows_test.rb
@@ -31,6 +31,33 @@ class ProfileFlowsTest < ActionDispatch::IntegrationTest
     assert_not_empty flash
   end
 
-  #  TODO Integration Flow Profile updated
-  # https://guides.rubyonrails.org/testing.html#system-testing
+  test 'should not update profile when first_name or last_name blank' do
+    sign_in @mike
+    @mike.create_profile(first_name: "Mike", last_name: "Freeman")
+
+    assert_equal(@mike.profile.first_name, "Mike")
+    assert_equal(@mike.profile.last_name, "Freeman")
+
+    assert_no_difference 'Profile.count' do
+      patch user_profile_path(@mike, @mike.profile), params: { profile: { first_name: '', last_name: 'Freeman' } }, xhr: true
+    end
+    assert_select 'li', /First name can't be blank/
+    assert_no_difference 'Profile.count' do
+      patch user_profile_path(@mike, @mike.profile), params: { profile: { first_name: 'Mike', last_name: '' } }, xhr: true
+    end
+    assert_select 'li', /Last name can't be blank/
+    assert_equal "update", @controller.action_name
+
+    assert_no_difference 'Profile.count' do
+      patch user_profile_path(@mike, @mike.profile), params: { profile: { first_name: 'Ben', last_name: 'Wallace', title: "Developer" } }, xhr: true
+    end
+
+    @mike.profile.reload
+    assert_equal(@mike.profile.first_name, "Ben")
+    assert_equal(@mike.profile.last_name, "Wallace")
+    assert_equal(@mike.profile.title, "Developer")
+
+    assert_redirected_to admin_profile_path
+    assert_not_empty flash
+  end
 end
--- test/system/profiles_test.rb
+++ test/system/profiles_test.rb
@@ -1,14 +1,31 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class ProfilesTest < ApplicationSystemTestCase
   def setup
     @mike = users(:mike)
   end
 
-  test "creating a profile" do
+  test 'creating a profile' do
     sign_in @mike
     visit admin_profile_url
     assert_selector 'h1', text: 'Dashboard - My Profile'
-    click_on 'Edit Profile'
+    click_on('Create Profile')
+
+    fill_in('First name', with: 'Willard')
+    fill_in('Last name', with: 'Acosta')
+    fill_in('Title', with: 'Project Manager')
+    fill_in('Company', with: 'Tesla')
+    fill_in('Phone', with: '554-523-0066')
+    fill_in('Address', with: '3251 Bernardo Street')
+    fill_in('City', with: 'Tampa')
+    fill_in('State', with: 'FL')
+    fill_in('Zip code', with: '10014')
+
+    click_button('Create Profile')
+
+    assert_text 'Your Profile was successfully created.'
+    assert_selector 'span', text: 'Project Manager'
   end
 end
